e9388f3bbc6b531ade138e33e34c5f6755c387b4
强辰 <qchen227@outlook.com>
2024-09-05 11:50:08 +0800

    优化shader文件

--- Runtime/Shader/HLSL/SurfaceDetail.hlsl
+++ Runtime/Shader/HLSL/SurfaceDetail.hlsl
@@ -2,7 +2,10 @@
 #define SURFACEDETAIL
 
 
+#include "Normal.hlsl"
 #include "Tex.hlsl"
+
+
 
 ///
 // 表面细节混合处理
@@ -72,5 +75,97 @@ out float3 N
 	// 法线
 	N = UnpackNormal(SampleTex(normalmap, uv));
 }
+
+
+
+
+// 表面法线细节混合
+void NormalBlend_float
+(
+// normal
+	float2 uv,
+	float2 uv3,
+	UnityTexture2D BaseNormal,
+	UnityTexture2D DetailNormal,
+	float normalforce,
+	float normalblendtype,
+// coat
+	UnityTexture2D CoatMap,
+	float coat,
+// blend
+	UnityTexture2D dirtRoughnessMap,
+	UnityTexture2D detailmaskMap,
+	float4 _BaseColor,
+	float4 _BaseColorOverlay,
+	float4 _BaseDirtColor,
+	float4 _DetailColor,
+	float _BaseSmoothness,
+	float _BaseDirtStrength,
+	float _BaseMetallic,
+	float _DetailEdgeWear,
+	float _DetailEdgeSmoothness,
+	float _DetailDirtStrength,
+	float _DetailOcclusionStrength,
+// output
+	out float3 N,
+	out float CoatMsk,
+	out float3 Albedo,
+	out float Metallic,
+	out float Smoothness,
+	out float Occ,
+	out float Alpha
+)
+{
+	// 法线
+	float3 basenormal = UnpackNormal(SampleTex(BaseNormal, uv3));
+	basenormal = lerp(float3(0,0,1), basenormal, normalforce);
+	float3 detailnormal = UnpackNormal(SampleTex(DetailNormal, uv));
+	BlendNormalDeal_float(detailnormal, basenormal, normalblendtype, N);
+
+	// 清漆
+	CoatMsk = SampleTex(CoatMap, uv).r * coat;
+
+	// 采样
+	float4 dirtRoughness_sample = SampleTex(dirtRoughnessMap, uv3);
+	float4 detailmask_sample = SampleTex(detailmaskMap, uv);
+
+	// 反照率计算
+	float clampResult77 = clamp( pow( dirtRoughness_sample.g , _BaseDirtStrength ) , 0.0 , 1.0 );
+	float4 lerpResult76 = lerp( _BaseDirtColor , float4( 1,1,1,0 ) , clampResult77);
+	float4 lerpResult71 = lerp( _BaseColor , _BaseColorOverlay , dirtRoughness_sample.r);
+	float4 lerpResult134 = lerp( lerpResult71 , _DetailColor , ceil( ( ( 1.0 - detailmask_sample.b ) + -0.95 ) ));
+	float temp_output_120_0 = ceil( ( detailmask_sample.b + -0.8 ) );
+	float4 lerpResult123 = lerp( lerpResult134 , float4( float3(1,0.95,0.9) , 0.0 ) , temp_output_120_0);
+	float clampResult49 = clamp( pow( ( detailmask_sample.r + 0.55 ) , 6.0 ) , 0.0 , 1.0 );
+	float4 lerpResult92 = lerp( lerpResult123 , ( lerpResult123 * clampResult49 ) , _DetailDirtStrength);
+	float clampResult62 = clamp( ( ( detailmask_sample.r + -0.55 ) * 2.0 ) , 0.0 , 1.0 );
+	float4 clampResult101 = clamp( ( clampResult62 + lerpResult92 ) , float4( 0,0,0,0 ) , float4( 1,1,1,0 ) );
+	float4 lerpResult32 = lerp( lerpResult92 , clampResult101 , _DetailEdgeWear);
+	Albedo = ( lerpResult76 * lerpResult32 ).rgb;
+
+	// 金属度
+	float lerpResult94 = lerp( 0.0 , clampResult62 , _DetailEdgeWear);
+	float clampResult97 = clamp( ( lerpResult94 + temp_output_120_0 ) , 0.0 , 1.0 );
+	Metallic = clamp( ( clampResult97 + _BaseMetallic ) , 0.0 , 1.0 );
+
+	// 平滑度
+	float4 lerpResult121 = 
+	lerp( ( ( max( clampResult62 , dirtRoughness_sample.a ) * lerpResult76 ) * _BaseSmoothness ) , _DetailEdgeSmoothness.xxxx , clampResult97);
+	Smoothness = lerpResult121.r;
+
+	// AO
+	Occ  =  lerp( 1.0 , detailmask_sample.g , _DetailOcclusionStrength);
+
+	// Alpha
+	Alpha = 1;
+}
+
+
+
+
+
+
+
+
 
 #endif //表面细节处理（由 Graphi 着色库工具生成）| 作者：强辰
