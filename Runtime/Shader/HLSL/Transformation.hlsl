82d25f0f40409ba77221cafb2e2a4b3d0ceb855f
CN.Graphi <qchen227@outlook.com>
2025-01-26 12:20:03 +0800

    增加向量相关计算的hlsl库文件以及优化 Transformation.hlsl 库文件

--- Runtime/Shader/HLSL/Transformation.hlsl
+++ Runtime/Shader/HLSL/Transformation.hlsl
@@ -38,4 +38,160 @@ float2 UVRot2( float r, float2 uv )
 
 
 
+// 3d旋转
+// In : 要旋转的向量或者点
+// Axis : 轴向
+// Radians : 弧度
+float3 RotateAxis(float3 In, float3 Axis, float Radians)
+{
+    float s = sin(Radians);
+    float c = cos(Radians);
+    float one_minus_c = 1.0 - c;
+
+    Axis = normalize(Axis);
+
+    float3x3 rot_mat = { 
+                            one_minus_c * Axis.x * Axis.x + c,            one_minus_c * Axis.x * Axis.y - Axis.z * s,     one_minus_c * Axis.z * Axis.x + Axis.y * s,
+                            one_minus_c * Axis.x * Axis.y + Axis.z * s,   one_minus_c * Axis.y * Axis.y + c,              one_minus_c * Axis.y * Axis.z - Axis.x * s,
+                            one_minus_c * Axis.z * Axis.x - Axis.y * s,   one_minus_c * Axis.y * Axis.z + Axis.x * s,     one_minus_c * Axis.z * Axis.z + c
+                        };
+
+    return mul(rot_mat,  In);
+}
+
+
+
+// 欧拉角（弧度）转四元数
+// radian : 弧度
+float4 RadiansToQuaternion(float3 radian)
+{
+    float3 r = radian;
+    r *= 0.5;
+
+    // 前缀c = cos， 前缀s = sin
+    float cyaw = cos(r.z); // 绕z
+    float syaw = sin(r.z);
+    float cpitch = cos(r.y); // 绕y
+    float spitch = sin(r.y);
+    float croll = cos(r.x); // 绕x
+    float sroll = sin(r.x);
+
+    // 计算四元数4个值
+    float x = cyaw * cpitch * sroll - syaw * spitch * croll;
+    float y = syaw * cpitch * sroll + cyaw * spitch * croll;
+    float z = syaw * cpitch * croll - cyaw * spitch * sroll;
+    float w = cyaw * cpitch * croll + syaw * spitch * sroll;
+                
+    return float4(x,y,z,w);
+}
+
+
+
+// 欧拉角转四元数
+// euler : 角度
+float4 EulerToQuaternion(float3 euler)
+{
+    return RadiansToQuaternion(radians(euler));
+}
+
+
+
+// 四元数转矩阵
+// q : 四元数
+float4x4 QuaternionToMatrix(float4 q)
+{
+    float num = q.x * 2;
+    float num2 = q.y * 2;
+    float num3 = q.z * 2;
+    float num4 = q.x * num;
+    float num5 = q.y * num2;
+    float num6 = q.z * num3;
+    float num7 = q.x * num2;
+    float num8 = q.x * num3;
+    float num9 = q.y * num3;
+    float num10 = q.w * num;
+    float num11 = q.w * num2;
+    float num12 = q.w * num3;
+
+    /*
+         创建矩阵，并计算16个值
+         [
+            M11, M12, M13, M14,
+            M21, M22, M23, M24,
+            M31, M32, M33, M34,
+            M41, M42, M43, M44
+         ]
+    */
+    float4x4 m = float4x4
+    (
+        1 - (num5 + num6),      num7 - num12,               num8 + num11,           0,
+        num7 + num12,           1 - (num4 + num6),          num9 - num10,           0,
+        num8 - num11,           num9 + num10,               1 - (num4 + num5),      0,
+        0,                      0,                          0,                      1
+    );
+
+    return m;
+}
+
+
+
+
+// 四元数相乘
+// q : 四元数
+// p : 向量或点
+float3 QuaternionMultiply(float4 q, float3 p)
+{
+    float num = q.x * 2;
+    float num2 = q.y * 2;
+    float num3 = q.z * 2;
+    float num4 = q.x * num;
+    float num5 = q.y * num2;
+    float num6 = q.z * num3;
+    float num7 = q.x * num2;
+    float num8 = q.x * num3;
+    float num9 = q.y * num3;
+    float num10 = q.w * num;
+    float num11 = q.w * num2;
+    float num12 = q.w * num3;
+
+    float3 result = float3
+    (
+        (1 - (num5 + num6)) * p.x + (num7 - num12) * p.y + (num8 + num11) * p.z,
+        (num7 + num12) * p.x + (1 - (num4 + num6)) * p.y + (num9 - num10) * p.z,
+        (num8 - num11) * p.x + (num9 + num10) * p.y + (1 - (num4 + num5)) * p.z
+    );
+    
+    return result;
+}
+
+
+
+
+// 世界矩阵背面的缩放因子
+float GetNegativeScale()
+{
+    return unity_WorldTransformParams.w >= 0.0 ? 1.0 : -1.0;
+}
+
+
+// 计算切线空间转世界空间矩阵
+// geomNormalWS : 顶点数据中的法线（世界空间）
+// geomTangentWS : 顶点数据中的切线（世界空间）
+float3x3 GetTangentToWorldMatrix(float3 geomNormalWS, float4 geomTangentWS)
+{
+    float3 unnormalizedNormalWS = geomNormalWS;
+    const float renormFactor = 1.0 / length(unnormalizedNormalWS);
+
+    // 双面渲染中支持翻转，这里需要确保切线和切线不会被翻转
+    float crossSign = (geomTangentWS.w > 0.0 ? 1.0 : -1.0) * GetNegativeScale();
+    float3 bitang = crossSign * cross(geomNormalWS.xyz, geomTangentWS.xyz);
+
+    float3 WorldSpaceNormal = renormFactor * geomNormalWS.xyz;
+    float3 WorldSpaceTangent = renormFactor * geomTangentWS.xyz;
+    float3 WorldSpaceBiTangent = renormFactor * bitang;
+
+    return float3x3(WorldSpaceTangent, WorldSpaceBiTangent, WorldSpaceNormal);
+}
+
+
 #endif //变换相关操作（由 Graphi 着色库工具生成）| 作者：强辰
---
+++ Runtime/Shader/HLSL/Vec.hlsl
@@ -0,0 +1,93 @@
+﻿#ifndef VEC
+#define VEC
+
+#include "Transformation.hlsl"
+
+
+// 获取绝对世界空间下的视角向量
+// posWS ： 顶点的绝对世界空间位置
+float3 GetAbsoluteViewWS(float3 posWS)
+{
+    return _WorldSpaceCameraPos.xyz - posWS;
+}
+
+
+// 获取绝对世界位置
+// posRWS : 相对摄像机的世界空间位置
+float3 GetAbsolutePosWS(float3 posRWS)
+{
+    return GetAbsolutePositionWS(posRWS);   
+}
+
+
+// BoxProjector 反射向量优化
+// reflectDir   : 反射方向
+// worldPos     : 绝对世界空间坐标
+// center       : BoxProjected 中心位置（xyz：位置，w的值若为1，则说明以Box形式计算；不为1，则直接返回反射向量）
+// boxMin       : BoxProjected 左下角位置
+// boxMax       : BoxProjected 右上角位置
+float3 BoxProjectedDirection(float3 reflectDir, float3 worldPos, float4 center, float3 boxMin, float3 boxMax)
+{
+    UNITY_BRANCH
+    if (center.w > 0.0) // w = 1.0 说明开启了boxprojector模式。
+    {
+        float3 nrdir = normalize(reflectDir);
+
+        #if 1
+            float3 rbmax = (boxMax.xyz - worldPos) / nrdir;
+            float3 rbmin = (boxMin.xyz - worldPos) / nrdir;
+            float3 rbminmax = (nrdir > 0.0f) ? rbmax : rbmin;
+
+        #else // 优化版本
+            float3 rbmax = (boxMax.xyz - worldPos);
+            float3 rbmin = (boxMin.xyz - worldPos);
+            float3 select = step (float3(0,0,0), nrdir);
+            float3 rbminmax = lerp (rbmax, rbmin, select);
+            rbminmax /= nrdir;
+
+        #endif
+
+        float fa = min(min(rbminmax.x, rbminmax.y), rbminmax.z);
+        worldPos -= center.xyz;
+        reflectDir = worldPos + nrdir * fa;
+    }
+    return reflectDir;
+}
+
+
+
+// 计算反射向量（世界空间）
+// gNormalWS : 顶点的世界空间法线
+// gTangentWS : 顶点的世界空间切线
+// dir : 方向
+// nTS : 法线（切线空间）
+float3 ReflectDirectionWS(float3 gNormalWS, float3 gTangentWS, float3 dir, float3 nTS)
+{
+    // 方向
+    if(!IsPerspectiveProjection())
+    {// 非透视矩阵
+        dir = GetViewForwardDir() * dot(dir, GetViewForwardDir());
+    }
+
+    // 法线
+    float3x3 tangentToWorldMatrix = GetTangentToWorldMatrix(gNormalWS, float4(gTangentWS, 1.0));
+    float3 nWS = TransformTangentToWorld(nTS.xyz, tangentToWorldMatrix, true);
+
+    // 反射方向
+    return reflect(-dir, nWS);
+}
+
+
+
+// 计算视角的反射向量（世界空间）
+// gNormalWS : 顶点的世界空间法线
+// gTangentWS : 顶点的世界空间切线
+// gPosRWS : 顶点的世界空间位置（相对摄像机）
+// nTS : 法线（切线空间）
+float3 ReflectDirectionOfViewWS(float3 gNormalWS, float3 gTangentWS, float3 gPosRWS, float3 nTS)
+{
+    float3 viewWS = GetAbsoluteViewWS(GetAbsolutePositionWS(gPosRWS)); // 绝对世界空间的视角向量
+    return ReflectDirectionWS(gNormalWS, gTangentWS, viewWS, nTS);
+}
+
+#endif //向量计算（由 Graphi 着色库工具生成）| 作者：强辰
